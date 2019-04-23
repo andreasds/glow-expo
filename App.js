4ad5d99310f92df9995e5c047c2e822c14e97307
Andreas D <andreas.ds90@gmail.com>
2019-04-23 17:41:34 +0700

    list all file in external storage

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Platform, StatusBar, Text, View } from 'react-native'
-import { ScreenOrientation } from 'expo'
+import { FileSystem, Permissions, ScreenOrientation } from 'expo'
 
 import { Provider } from 'react-redux'
 import store from './android/redux/store'
@@ -8,15 +8,22 @@ import store from './android/redux/store'
 import RootNavigator from './android/components/navigations/RootNavigator'
 
 class App extends Component {
-  componentDidMount() {
+  async componentDidMount() {
     ScreenOrientation.allowAsync("LANDSCAPE_LEFT")
+
+    if (Platform.OS === 'android') {
+      const result = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+      console.log(Expo.FileSystem.documentDirectory)
+      const files = await FileSystem.readDirectoryAsync('file:///storage/emulated/0/')
+      console.log(files)
+    }
   }
 
   render() {
     if (Platform.OS === 'android') {
       return (
         <Provider store={store}>
-          <StatusBar barStyle='light-content' hidden={true}  />
+          <StatusBar barStyle='light-content' hidden={true} />
           <RootNavigator />
         </Provider>
       )
