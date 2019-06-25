9492ee17222a283429c5d499d2b7987bd3c98540
Andreas D <andreas.ds90@gmail.com>
2019-06-25 15:30:10 +0700

    create stack navigator

---
+++ src/android/components/navigations/RootNavigator.js
@@ -0,0 +1,29 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+// import { connect } from 'react-redux'
+
+import HomeScreen from '../screens/HomeScreen'
+// import CustomerNavigator from '../navigations/CustomerNavigator'
+// import EmployeeNavigator from '../navigations/EmployeeNavigator'
+// import SummaryScreen from '../screens/SummaryScreen'
+// import TreatmentNavigator from '../navigations/TreatmentNavigator'
+
+const RootNavigator = createStackNavigator(
+    {
+        Home: HomeScreen,
+        // Customer: CustomerNavigator,
+        // Employee: EmployeeNavigator,
+        // Summary: SummaryScreen,
+        // Treatment: TreatmentNavigator
+    }
+)
+
+RootNavigator.navigationOptions = () => {
+    let navigationOptions = {}
+    navigationOptions.initialRouteName = 'Home'
+    return navigationOptions
+}
+
+const getAppNavigator = createAppContainer(RootNavigator)
+
+// export default connect()(getAppNavigator)
+export default getAppNavigator
